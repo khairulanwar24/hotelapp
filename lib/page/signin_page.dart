554383df7bb235696203c7d8856568f4c20deb2d
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-01-02 21:19:40 +0700

    user user_source

--- lib/page/signin_page.dart
+++ lib/page/signin_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hotelkhan/config/app_asset.dart';
 import 'package:hotelkhan/config/app_color.dart';
-import 'package:hotelkhan/config/app_route.dart';
-import 'package:hotelkhan/page/home_page.dart';
 import 'package:hotelkhan/widget/button_custom.dart';
 
 class SignInPage extends StatelessWidget {
   SignInPage({super.key});
   final controllerEmail = TextEditingController();
   final controllerPassword = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
-  login(BuildContext context) {}
+  login(BuildContext context) {
+    if (formKey.currentState!.validate()) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,87 +21,98 @@ class SignInPage extends StatelessWidget {
           return SingleChildScrollView(
             child: ConstrainedBox(
               constraints: BoxConstraints(minHeight: constraints.maxHeight),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      AppAsset.logo,
-                      fit: BoxFit.fitWidth,
-                    ),
-                    SizedBox(height: 80),
-                    Text(
-                      'Sign In\nTo Your Account',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        AppAsset.logo,
+                        fit: BoxFit.fitWidth,
+                      ),
+                      const SizedBox(height: 80),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Sign In\nTo Your Account',
+                          style:
+                              Theme.of(context).textTheme.titleLarge!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        controller: controllerEmail,
+                        validator: (value) =>
+                            value == '' ? "Don't empty" : null,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 16),
+                          hintText: 'Email Address',
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                    ),
-                    SizedBox(height: 30),
-                    TextFormField(
-                      controller: controllerEmail,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 16),
-                        hintText: 'Email Address',
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              const BorderSide(color: AppColor.secondary),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: AppColor.secondary),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      controller: controllerPassword,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 16),
-                        hintText: 'Password',
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              const BorderSide(color: AppColor.secondary),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: controllerPassword,
+                        validator: (value) =>
+                            value == '' ? "Don't empty" : null,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 16),
+                          hintText: 'Password',
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: AppColor.secondary),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    ButtonCustom(
-                      label: 'Sign In',
-                      isExpand: true,
-                      onTap: () => login(context),
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    Text(
-                      'Create New Account',
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ButtonCustom(
+                        label: 'Sign In',
+                        isExpand: true,
+                        onTap: () => login(context),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      const Text(
+                        'Create New Account',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
