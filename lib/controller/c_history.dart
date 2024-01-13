cbf41da7908441df9ef0a67eae4ca126f6980760
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-01-14 02:05:10 +0700

    Booking Source History

---
+++ lib/controller/c_history.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:hotelkhan/model/booking.dart';
+import 'package:hotelkhan/source/booking_source.dart';
+
+class CHistory extends GetxController {
+  final _listHistory = <Booking>[].obs;
+  List<Booking> get listHistory => _listHistory.value;
+
+  getListHotel(String id) async {
+    _listHistory.value = await BookingSource.getHistory(id);
+    update();
+  }
+}
